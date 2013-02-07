673ef92684c2a214a46bf0c510091cf336bb8894
Petja <petja@petjatouru.com>
2013-02-07 19:08:19 +0200

    Grafiikan iterointi
    Taustagrafiikan iterointi kolmelle riville ja viiteen sarakkeeseen

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 $(function(){
 	var game = $("canvas");
-	game.drawImage({
-		source: "img/test.png",
-		x: 0, y: 0
-	});
+	for(ia=0;ia<3;ia++){ // Iteroi grafiikka kolmelle eri riville
+		for(ib=0;ib<5;ib++){ // Iteroi grafiikka viiteen eri sarakkeeseen
+			game.drawImage({ // Piirrä grafiikka
+				source: "img/test.png",
+				x: ib*192, y: ia*192,
+				fromCenter: false
+			});
+		}
+	}
 });
